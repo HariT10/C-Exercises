b5b233aa64088c699fbc1b3f45c8cbe1a68a728d
Harishan Thilakanathan <79531856+HariT10@users.noreply.github.com>
2023-12-15 23:49:51 -0500

    Add files via upload

---
+++ cube.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+//By: Harishan Thilakanathan
+//Purpose:
+//Date: Friday, Decemeber 15, 2023
+
+int main(){
+
+    int base;
+
+    printf("Please Enter Any Number That You Would Like the Cube of: ");
+    scanf("%d", &base);
+    
+    int result = cube(base);
+
+
+    printf("The Cube of %d is %d", base, result);
+
+
+
+}
+
+
+int cube(int base){
+    int base;
+    int cubedNumber;
+
+    cubedNumber = base * base * base;
+
+    return cubedNumber;
+
+}
+
